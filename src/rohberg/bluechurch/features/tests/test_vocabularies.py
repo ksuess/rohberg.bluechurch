6ecd44680d1b40b8e0a92702462ca8f6336815ba
Katja Süss <k.suess@rohberg.ch>
2017-09-28 18:15:42 +0200

    BluechurchTags

---
+++ src/rohberg/bluechurch/features/tests/test_vocabularies.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from zope.component import queryUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
+from plone import api
+from rohberg.bluechurch.features.testing import ROHBERG_BLUECHURCH_FEATURES_INTEGRATION_TESTING  # noqa
+
+import unittest
+
+
+class TestSetup(unittest.TestCase):
+    """Test that rohberg.bluechurch.features is properly installed."""
+
+    layer = ROHBERG_BLUECHURCH_FEATURES_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        # self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_values(self):
+        bctags = queryUtility(IVocabularyFactory, name='rohberg.bluechurch.features.BluchurchTags')
+        self.assertTrue(isinstance(bctags(), SimpleVocabulary))
+
---
+++ src/rohberg/bluechurch/features/vocabularies.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+def BluchurchTagsVocabularyFactory(context=None):
+    terms = []
+    terms.append(SimpleVocabulary.createTerm("kinder", "kinder", u"Kinder"))
+    terms.append(SimpleVocabulary.createTerm("madchen", "madchen", u"Mädchen"))
+    terms.append(SimpleVocabulary.createTerm("senioren", "senioren", u"Senioren"))
+    return SimpleVocabulary(terms)
