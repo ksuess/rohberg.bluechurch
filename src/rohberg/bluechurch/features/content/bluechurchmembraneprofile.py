6d84b340d1aa806fcb186721b1ab37ee8be18e35
Katja Suss <k.suess@rohberg.ch>
2017-10-24 10:34:30 +0200

    Profile Artist

--- src/rohberg/bluechurch/features/content/bluechurchmembraneprofile.py
+++ src/rohberg/bluechurch/features/content/bluechurchmembraneprofile.py
@@ -1,21 +1,11 @@
-# TODO: IBluechurchmembraneprofile mit Schema
-# siehe dexterity.membrane.content.member.IMember
-
 from zope import schema
+from plone.supermodel import model
 from dexterity.membrane.content.member import IMember
-from rohberg.bluechurch import _
+from rohberg.bluechurch.features import _
 
 class IBluechurchmembraneprofile(IMember):
     """
     Artist or Event Manager
-    TODO: Username automatisch konstruieren aus Vor- und Nachname
     """
-
-    username = schema.ASCIILine(
-        title=_(u"Username"),
-        description=_(u"Enter a user name, usually something like 'jsmith'."
-            " No spaces or special characters. Usernames and passwords are "
-            "case sensitive, make sure the caps lock key is not enabled. "
-            "This is the name used to log in."),
-        required=True,
-    )
+    model.load('bluechurchmembraneprofile.xml')
+    
