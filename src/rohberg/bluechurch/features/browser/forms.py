3526a221524b6278504b3e66fe1f998d0b8fa53c
Katja Suss <k.suess@rohberg.ch>
2017-10-24 16:21:39 +0200

    Add Profile nur für Manager Site Administrator und Anonymous

--- src/rohberg/bluechurch/features/browser/forms.py
+++ src/rohberg/bluechurch/features/browser/forms.py
@@ -1,5 +1,7 @@
 from Acquisition import aq_inner
+from AccessControl import Unauthorized
 from zope.component import getUtility
+from plone import api
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.dexterity.utils import addContentToContainer
 from plone.dexterity.browser import add
@@ -9,11 +11,26 @@ class AddForm(add.DefaultAddForm):
     
     def update(self):
         # TODO: check here if the user is anonymous and raise exception if not
+        is_manager = api.user.has_permission('Manage portal')
+        is_sitemanager = False
+        if not(api.user.is_anonymous() or is_manager):
+            current = api.user.get_current()
+            user = current
+            username = user.getName()
+            roles = api.user.get_roles(username=username)
+            is_sitemanager = "Site Administrator" in roles
+        if not(api.user.is_anonymous() or is_manager or is_sitemanager):
+            raise Unauthorized('You are already registered.')
         super(AddForm, self).update()
+        
+        
+
+
 
     def add(self, object):
+        """ TODO: Profile nur in Folder "profiles"
         
-        print("** add method of custom AddForm")
+        """
         
         fti = getUtility(IDexterityFTI, name=self.portal_type)
         container = aq_inner(self.context)
