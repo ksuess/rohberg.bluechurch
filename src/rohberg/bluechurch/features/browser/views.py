a63ca8c9200ad82132c95794376caf9cc58aeb71
Katja Suss <k.suess@rohberg.ch>
2017-10-24 16:20:18 +0200

    Testview

--- src/rohberg/bluechurch/features/browser/views.py
+++ src/rohberg/bluechurch/features/browser/views.py
@@ -1,6 +1,7 @@
 from zope.component import queryUtility
 from zope.schema.interfaces import IVocabularyFactory
 
+from plone import api
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.dexterity.browser.view import DefaultView
@@ -10,18 +11,25 @@ from rohberg.bluechurch.features import _
 class BluechurchprofileView(DefaultView):
     """ the default view for BluechurchProfile"""
 
-# Testview
-class MyView(BrowserView):
+    # def allbctags(self):
+    #     bctags = queryUtility(IVocabularyFactory, name='rohberg.bluechurch.features.BluchurchTags')
+    #     # import pdb; pdb.set_trace()
+    #     result = bctags()(self.context).__dict__
+    #     return result
 
-    # index = ViewPageTemplateFile("my-template.pt")
 
-    # def render(self):
-    #     return self.index()
-
-    def __call__(self):
-        # return self.render()
-        bctags = queryUtility(IVocabularyFactory, name='rohberg.bluechurch.features.BluchurchTags')
-        # import pdb; pdb.set_trace()
-        result = bctags()(self.context).__dict__
-        return result
-
+class TestView(BrowserView):
+    """ Testing utilities"""
+    
+    def wanttoknow(self):
+        is_manager = api.user.has_permission('Manage portal')
+        if is_manager:
+            return "I am Manager"
+        current = api.user.get_current()
+        user = current
+        username = user.getName()
+        roles = api.user.get_roles(username=username)
+        fullname = user.getProperty('fullname')
+        email = user.getProperty('email')
+        home_page = user.getProperty('home_page')
+        return email
