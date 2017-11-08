49b7a0dc0c8a7f74eae623a0add766801413f7a3
Katja Suss <k.suess@rohberg.ch>
2017-11-08 17:03:47 +0100

    cleanup after repository fails

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,11 +78,8 @@ module.exports = function (grunt) {
                 options: {
                     watchTask: true,
                     debugInfo: true,
-<<<<<<< HEAD
                     proxy: "localhost:10680",
-=======
-                    proxy: "localhost:8080",
->>>>>>> origin/master
+
                     reloadDelay: 3000,
                     // reloadDebounce: 2000,
                     online: true
