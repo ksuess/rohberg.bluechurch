da37c49ecf1f9ab3364cfbeb2deb050ce5bf6863
Katja Süss <k.suess@rohberg.ch>
2017-11-25 23:48:17 +0100

    Snippetsviewlet: user_actions, document_actions

--- src/rohberg/bluechurch/browser/viewlets.py
+++ src/rohberg/bluechurch/browser/viewlets.py
@@ -1,4 +1,7 @@
 from plone.app.layout.viewlets import common as base
+from plone import api
+from plone.app.content.interfaces import INameFromTitle
+
 
 class UserActionsViewlet(base.ViewletBase):
     """ Become a member
@@ -9,3 +12,25 @@ class UserActionsViewlet(base.ViewletBase):
         """ TODO: isMember
         """
         return True
+        
+
+
+class SnippetsViewlet(base.ViewletBase):
+    """ diverse Snippets
+
+    """
+    @property
+    def current_user(self):
+        result = {}
+        
+        current = api.user.get_current()
+        current_profile = api.content.get(UID=current.id)
+        result['url'] = current_profile and current_profile.absolute_url() or "#"
+        result['fullname'] = INameFromTitle(current_profile).title
+        return result
+        
+    @property
+    def can_edit(self):
+        current = api.user.get_current()
+        local_roles = api.user.get_roles(user=current, obj=self.context, inherit=False)
+        return "Editor" in local_roles
