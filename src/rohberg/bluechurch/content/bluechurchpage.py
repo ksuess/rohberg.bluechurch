03f46a83009b09ca9b730fc95e429a736a0995c9
Katja Süss <k.suess@rohberg.ch>
2017-11-26 13:52:52 +0100

    page (research)

--- src/rohberg/bluechurch/content/bluechurchpage.py
+++ src/rohberg/bluechurch/content/bluechurchpage.py
@@ -5,12 +5,15 @@ from zope.interface import implements
 from plone import api
 from plone.app.vocabularies.catalog import CatalogSource
 from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
 from plone.autoform.directives import widget
 from plone.dexterity.content import Item
 from plone.supermodel import model
 from zope.interface import implementer
 
 from rohberg.bluechurch.content.interfaces import IBluechurchMemberContent
+from rohberg.bluechurch.utils import get_profiles_base_path, get_locations_base_path
 
 import logging
 logger = logging.getLogger(__name__)
@@ -22,6 +25,24 @@ class IBluechurchpage(model.Schema):
     """ Marker interface for Bluechurchpage
     """
     
+    beteiligte = RelationList(
+        title=_(u"Artists"),
+        description=_(u"Beteiligte Artists, Veranstalter"),
+        required=False,
+        value_type=RelationChoice(
+                vocabulary='plone.app.vocabularies.Catalog',
+                )
+        )
+    widget(
+        'beteiligte',
+        RelatedItemsFieldWidget,
+        pattern_options={
+            'selectableTypes': ['dexterity.membrane.bluechurchmembraneprofile',],
+            'mode': 'search',
+            'basePath': get_profiles_base_path,
+        }
+        )
+        
     model.load('bluechurchpage.xml')
 
 
