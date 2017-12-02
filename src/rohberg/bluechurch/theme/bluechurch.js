4961ce466b8ee09ebf883062bb35ff03f59e056a
Katja Süss <k.suess@rohberg.ch>
2017-12-02 13:43:03 +0100

    Audio: related items

--- src/rohberg/bluechurch/theme/bluechurch.js
+++ src/rohberg/bluechurch/theme/bluechurch.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-
+    
     var random_22 = Math.floor((Math.random() * 22) + 1);
     $("#visual-portal-wrapper")
         .css("background-image", "url('/++theme++bluechurch/images/backgrounds/blue_church_" + random_22 + ".jpg')");
+
+    
+    // var related_audio = $(".relatedItems a[href$='mp3']").each(function( index ) {
+    var extensions = ["mp3", "wma", "wav", "m4a", "m4b", "m4p", "mpc", "aiff"];
+    for (i = 0; i < extensions.length; i++) {
+        var related_audio = $('.relatedItems a[href$="'+ extensions[i] +'"]').each(function( index ) {
+            var filename = $(this).attr('href').split("/").pop();
+            $(this).parent().html($(this).html() +'<br><audio src="'+ filename +'" controls="controls"></audio>');
+        });
+    } 
+
+    
     
 });
