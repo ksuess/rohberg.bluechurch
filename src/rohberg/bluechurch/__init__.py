e43fd7af6d9f90b83a8062a6db7691feb2686398
Katja Süss <k.suess@rohberg.ch>
2017-12-06 10:20:43 +0100

    location: set address required

--- src/rohberg/bluechurch/__init__.py
+++ src/rohberg/bluechurch/__init__.py
@@ -5,3 +5,12 @@ __import__('pkg_resources').declare_namespace(__name__)
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('rohberg.bluechurch')
+
+
+
+
+from collective.address.behaviors import IAddress
+
+IAddress['zip_code'].required = True
+IAddress['city'].required = True
+IAddress['country'].required = True
