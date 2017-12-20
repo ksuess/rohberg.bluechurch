8924f8eb14901559ed66208fc7e19a1d227fcce3
Katja Süss <k.suess@rohberg.ch>
2017-12-20 08:15:12 +0100

    City-Filter collapsed

--- src/rohberg/bluechurch/theme/bluechurch.js
+++ src/rohberg/bluechurch/theme/bluechurch.js
@@ -29,14 +29,25 @@ $(document).ready(function() {
     $("html[lang='de'] label[for='form-widgets-ILeadImage-image']").text("Profilbild");
     $("html[lang='de'] label[for='form-widgets-ILeadImage-image_caption']").text("Legende zum Profilbild");
     
-    $("aside.filterCity .portletContent").hide();
-    $('aside.filterCity .portletHeader').css({"cursor":"pointer","color": "#81b7e6"});
-    $('aside.filterCity .portletHeader').prepend("<span class='open-down'></span>");
+    // Collapse City Filter
+    // $("aside.filterCity .portletContent").hide();
+    // $('aside.filterCity .portletHeader').css({"cursor":"pointer","color": "#81b7e6"});
+    // $('aside.filterCity .portletHeader').prepend("<span class='open-down'></span>");
 
-    $("aside.filterCity .portletHeader" ).click(function() {
-      $("aside.filterCity .portletContent").slideToggle( function() {
+
+    var portlet = $("aside.filterCity")
+    var pwrapper = portlet.parent().addClass("portletWrapperCollapsible");    
+    var pheader = portlet.find(".portletHeader");
+    portlet.wrap( "<div class='innerPortletwrapper'></div>" );
+    var iwrapper = pwrapper.find(".innerPortletwrapper");
+    pheader.addClass("portletHeaderCollapsible").insertBefore(iwrapper);
+    
+
+    pheader.click(function() {
+      iwrapper.slideToggle( function() {
         // Animation complete.
       });
     });
+    
     
 });
