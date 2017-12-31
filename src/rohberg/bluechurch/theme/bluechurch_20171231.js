2b78f427ce9bdf040adc242c60edf05b0ab11614
Katja Süss <k.suess@rohberg.ch>
2017-12-31 11:07:13 +0100

    change bluechurch.js

---
+++ src/rohberg/bluechurch/theme/bluechurch_20171231.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+    
+    // background images    
+    var random_17 = Math.floor((Math.random() * 17) + 1); // Zahl von 1 bis 17
+    
+    // media query event handler
+    if (matchMedia) {
+      const mq = window.matchMedia("(min-width: 480px)");
+      mq.addListener(WidthChange);
+      WidthChange(mq);
+    }
+
+    // media query change
+    function WidthChange(mq) {
+        if (mq.matches) {
+            $("#visual-portal-wrapper")
+                .css("background-image", "url('/++theme++bluechurch/images/backgrounds/blue_church_" + random_17 + ".jpg')");
+        } else {
+            $("#visual-portal-wrapper")
+                .css("background-image", "url('/++theme++bluechurch/images/backgrounds_small/blue_church_" + random_17 + "_resized.jpg')");
+        }
+    }
+    // end background images
+    
+    // "related item" als audio player anzeigen, wenn mp3 oder aehnliches
+    var extensions = ["mp3", "mp4", "wma", "wav", "m4a", "m4b", "mpc", "aiff"];
+    // "m4p", 
+    for (i = 0; i < extensions.length; i++) {
+        var related_audio = $('.relatedItems a[href$="'+ extensions[i] +'"]').each(function( index ) {
+            var filename = "/"+$(this).attr('href').split("/").slice(3).join("/");
+            $(this).parent().html($(this).html() +'<br><audio src="'+ filename +'" controls="controls"></audio>');
+        });
+    } ;
+    
+    // Links auf Audio-Files in Player umwandeln
+    var extensions = ["mp3", "mp4", "wma", "wav", "m4a", "m4b", "mpc", "aiff"];
+    // "m4p",
+    var snip = '<audio src="mysrc" controls="controls"></audio>'
+    for (i = 0; i < extensions.length; i++) {
+        $("body").not("body.template-file_view")
+            .find('a[href$=".' + extensions[i] + '"]').each(function() {
+                var href = $(this).attr('href');
+                // $(this).parent().html(snip.replace('mysrc',href));
+                $(this).replaceWith(snip.replace('mysrc',href));
+        })
+    } ;
+    
+ 
+    // collective.collectionfilter
+    $('.filterLabel').each(function(index) {
+        var txt = $(this).text();
+        $(this).text(txt.replace(/-/g, " "));
+    });
+    
+    
+    $("label[for='__ac_name'], label[for='userid']").text("Email");
+    
+    // Translations
+    // $("html[lang='de']");
+    $("html[lang='de'] label[for='form-widgets-ILeadImage-image']").text("Profilbild");
+    $("html[lang='de'] label[for='form-widgets-ILeadImage-image_caption']").text("Legende zum Profilbild");
+    
+    // Collapse City Filter
+    // $("aside.filterCity .portletContent").hide();
+    // $('aside.filterCity .portletHeader').css({"cursor":"pointer","color": "#81b7e6"});
+    // $('aside.filterCity .portletHeader').prepend("<span class='open-down'></span>");
+
+
+    var portlet = $("aside.filterCity")
+    var pwrapper = portlet.parent().addClass("portletWrapperCollapsible");    
+    var pheader = portlet.find(".portletHeader");
+    portlet.wrap( "<div class='innerPortletwrapper'></div>" );
+    var iwrapper = pwrapper.find(".innerPortletwrapper");
+    pheader.addClass("portletHeaderCollapsible").insertBefore(iwrapper);
+    
+
+    pheader.click(function() {
+      iwrapper.slideToggle( function() {
+        // Animation complete.
+      });
+    });
+    
+    
+});
