201370bf5da6138e5cb87e2a673f6489ec8e7141
Katja Süss <k.suess@rohberg.ch>
2018-02-09 17:25:03 +0100

    File Size Validator for Image and File Fields
    Betrifft alle Felder in allen Typen

---
+++ src/rohberg/bluechurch/browser/validators.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+from AccessControl import getSecurityManager
+from plone import api
+from plone.namedfile.interfaces import INamedBlobImageField
+from plone.namedfile.interfaces import INamedBlobFileField
+# from plone.app.contenttypes.interfaces import IImage
+from zope.interface import Invalid
+from z3c.form import validator
+
+
+from rohberg.bluechurch import _
+
+class FileFileSizeValidator(validator.FileUploadValidator):
+
+    def validate(self, value):
+        super(FileFileSizeValidator, self).validate(value)
+
+        portal = api.portal.get()
+        
+        # if Site Manager: no limit
+        sm = getSecurityManager()
+        if sm.checkPermission('Add portal member', portal):
+            pass
+        else:
+            maximumSize = api.portal.get_registry_record('rohberg.bluechurch.maxFileSize')           
+            msgid = _(u"file_too_large_msg", default=u"File is too large (more than ${result} MB)", 
+                mapping={u"result": "{:.1f}".format(float(maximumSize)/1000)})
+            translated = self.context.translate(msgid)            
+            if value.getSize() > maximumSize*1000:
+                raise Invalid(translated)
+
+
+class ImageFileSizeValidator(validator.FileUploadValidator):
+
+    def validate(self, value):
+        super(ImageFileSizeValidator, self).validate(value)
+        portal = api.portal.get()
+        
+        # if Site Manager: no limit
+        sm = getSecurityManager()
+        if sm.checkPermission('Add portal member', portal):
+            pass
+        else: 
+            maximumSize = api.portal.get_registry_record('rohberg.bluechurch.maxImageSize')            
+            msgid = _(u"image_too_large_msg", default=u"Image is too large (more than ${result} MB)", 
+                mapping={u"result": "{:.1f}".format(float(maximumSize)/1000)})
+            translated = self.context.translate(msgid)            
+            if value.getSize() > maximumSize*1000:
+                raise Invalid(translated)
+
+
+validator.WidgetValidatorDiscriminators(FileFileSizeValidator,
+                                        field=INamedBlobFileField)
+
+validator.WidgetValidatorDiscriminators(ImageFileSizeValidator,
+                                        # context=IImage,
+                                        field=INamedBlobImageField)
+                                        
+
+
+
+
