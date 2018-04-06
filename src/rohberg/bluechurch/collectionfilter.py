45b579641eaba4e7e1d7ecf7295bef7dccf73cf7
Katja Süss <k.suess@rohberg.ch>
2018-04-06 19:17:18 +0200

    Bug Fix collectionfilter display_modifier for country (dirty hack)
    first request throws KeyError 'country'

--- src/rohberg/bluechurch/collectionfilter.py
+++ src/rohberg/bluechurch/collectionfilter.py
@@ -9,10 +9,24 @@ from plone import api
 from collective.collectionfilter.interfaces import IGroupByCriteria
 from collective.collectionfilter.interfaces import IGroupByModifier
 
+import logging
+logger = logging.getLogger(__name__)
 
 @implementer(IGroupByModifier)
 @adapter(IGroupByCriteria)
 def groupby_modifier(groupby):
+    """ todo: collectionfilter display_modifier for country"""
     factory = getUtility(IVocabularyFactory, 'collective.address.CountryVocabulary')
     countryvocabulary = factory(api.portal.get())
-    groupby._groupby['country']['display_modifier'] = lambda it: countryvocabulary.getTerm(it).title
+    try:
+        groupby._groupby['country']['display_modifier'] = lambda it: countryvocabulary.getTerm(it).title
+    except KeyError as  e:
+        logger.error(str(e))
+    
+    # groupby._groupby['Subject']['display_modifier'] = lambda x: x.upper()
+    # groupby._groupby['my_new_index'] = {
+    #     'index': 'my_new_index',
+    #     'metadata': 'my_new_index_metadata_colum',
+    #     'display_modifier': lambda it: u'this is awesome: {0}'.format(it)
+    # }
+    #
